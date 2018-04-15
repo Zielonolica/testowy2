20645631f513edbd8b57c8efb81f68affa7e7402
S22IP103GA05\Uczeń <jarzebczyk.kamila@gmail.com>
2018-04-15 15:59:10 +0200

    matma

--- nowyprojekt/venv/macierz.py
+++ nowyprojekt/venv/macierz.py
@@ -6,3 +6,23 @@ def pokazMacierz():
             macierz[i][j] = i * j
             print(macierz[i][j], end = ' ')
 #pokazMacierz()
+
+def dodawanie(x, y):
+    print(x + y)
+
+def odejmowanie(x, y):
+    roznica = x - y
+    return roznica
+
+def mnozenie(x, y):
+    iloczyn = x * y
+    return iloczyn
+
+def dzielenie(x, y):
+    iloraz = x / y
+    return iloraz
+
+dodawanie(5, 4)
+print(odejmowanie(7, 5))
+print(mnozenie(3, 5))
+print(dzielenie(6, 2))
--- nowyprojekt/venv/mainscript.py
+++ nowyprojekt/venv/mainscript.py
@@ -3,3 +3,5 @@
 import macierz
 
 macierz.pokazMacierz()
+
+
