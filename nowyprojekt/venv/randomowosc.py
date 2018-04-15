ac5d54fc1b1c7e9c43e545a02ca3bc747ca392c2
S22IP103GA05\Uczeń <jarzebczyk.kamila@gmail.com>
2018-04-15 16:59:09 +0200

    randomowe pary

--- nowyprojekt/venv/randomowosc.py
+++ nowyprojekt/venv/randomowosc.py
@@ -1,9 +1,10 @@
 import random
 import time
 
-listaImion = ["kamila", "arek", "piotrek", "robert", "bartek"]
+listaImion = ["kamila", "arek", "piotrek", "robert", "bartek", "grazynka"]
 
 random.seed(time.time())
 liczba = random.randrange(0, 11)
 print(liczba)
-print(random.choice(listaImion)) 
+print(random.choice(listaImion))
+print(random.sample(listaImion, 2))
