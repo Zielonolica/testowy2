56f7b8449f437615d75f97156521fc69006ab358
S22IP103GA05\Uczeń <jarzebczyk.kamila@gmail.com>
2018-04-15 16:21:07 +0200

    matma2

--- nowyprojekt/venv/mainscript.py
+++ nowyprojekt/venv/mainscript.py
@@ -1,7 +1,11 @@
 #import inwokacja
 #import listy
 import macierz
+import matma
 
 macierz.pokazMacierz()
 
-
+matma.dodawanie(5, 7)
+matma.odejmowanie(7, 5)
+matma.mnozenie(2, 5)
+matma.dzielenie(10, 5)
---
+++ nowyprojekt/venv/matma.py
@@ -0,0 +1,11 @@
+def dodawanie(x, y):
+    print(x + y)
+
+def odejmowanie(x, y):
+    print(x - y)
+
+def mnozenie(x, y):
+    print(x * y)
+
+def dzielenie(x, y):
+    print(x / y)
